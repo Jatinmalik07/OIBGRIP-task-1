3c906a1370f815690060ff3af98dab4d889c1e6d
jatin malik <90783190+Jatinmalik07@users.noreply.github.com>
2022-09-07 01:17:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) 
+{
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+};
+
+const bar = document.querySelector(".bar");
+const nav = document.querySelector(".navbar");
+const info = document.querySelector(".info")
+bar.addEventListener('click',() => {
+    nav.classList.toggle('active');
+    info.classList.toggle('active1');
+})
+
